3a62480c936eebc92d1c269b9a319fad0acfb23c
al-badarin <jamalalbadarin@gmail.com>
2025-08-25 09:27:39 +0300

    Enable routing and import ProjectDetail page
    Uncommented the import for ProjectDetail and added basic route definitions for Home, ProjectDetail, and NotFound components using react-router-dom.

--- projects-carousel/src/App.jsx
+++ projects-carousel/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom';
 // import Carousel from './components/Carousel.jsx';
 // import ProjectCard from './components/ProjectCard.jsx';
-// import ProjectDetail from './pages/ProjectDetail.jsx';
+import ProjectDetail from './pages/ProjectDetail.jsx';
 // import { projects } from './data/projects.js';
 
 function App() {
@@ -16,6 +16,13 @@ function App() {
         </div>
       </header>
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project/:id" element={<ProjectDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
