e0692c74d695234f8d67a87743f3c3d058525c63
al-badarin <jamalalbadarin@gmail.com>
2025-08-25 09:31:37 +0300

    Enable Carousel and projects data imports in App.jsx
    Uncommented imports for Carousel component and projects data in App.jsx to activate their usage in the application.

--- projects-carousel/src/App.jsx
+++ projects-carousel/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Link } from 'react-router-dom';
-// import Carousel from './components/Carousel.jsx';
+import Carousel from './components/Carousel.jsx';
 import ProjectCard from './components/ProjectCard.jsx';
 import ProjectDetail from './components/ProjectDetail.jsx';
-// import { projects } from './data/projects.js';
+import { projects } from './data/projects.js';
 
 function App() {
   return (
